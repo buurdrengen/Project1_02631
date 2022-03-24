ecc8e1aa5a274cd283921efb25e3a61c79c84ab3
buurdrengen <s203947@student.dtu.dk>
2022-03-24 08:55:44 +0100

    Files&Githubguide

--- startscript.py
+++ startscript.py
@@ -1,1 +1,8 @@
 ## Get started ## 
+
+# GitHub guide
+
+# Update a file/script you are working on. 
+# Open GitHub, write a summary for the changes you made (nothing big)
+# Pull changes if there is any
+# Push your own changes. 
