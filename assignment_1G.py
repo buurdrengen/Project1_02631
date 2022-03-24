57900b178d3b0ac90bcd7ec65ec25d4359664843
cglitz-dtu <cglitz@biosustain.dtu.dk>
2022-03-24 09:14:53 +0100

    Create assignment_1G.py

---
+++ assignment_1G.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  9 18:50:22 2022
+
+@author: cglitz
+"""
+import math
+
+b = 12
+c = 10
+A = 0.25*math.pi
+a = math.sqrt((b**2+c**2-2*b*c*math.cos(A)))
